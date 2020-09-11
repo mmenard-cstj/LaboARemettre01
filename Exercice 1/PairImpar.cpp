bd6746e9d0d6c7e1721661ef9795dca12b3b0dbc
Matthew Ménard <2066734@cstj.qc.ca>
2020-09-11 15:59:14 -0400

    Ajoutez des fichiers projet.

---
+++ Exercice 1/PairImpar.cpp
@@ -0,0 +1,24 @@
+//but:Déterminer si le chiffre entré est pair ou impair
+//auteur:Matthew Menard
+//date:2020-09-11
+
+
+//biblio cout/cin
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, ""); // convertie les accents à windows
+
+	//déclaration de variables
+	int nombre1;
+
+	//invite à entrer un nombre entier
+	std::cout << "veuillez entrer un nombre entier:";
+	// le prog va lire le nombre tapé au clavier et le stcker dans la ram
+	std::cin >> nombre1;
+
+
+
+
+}
